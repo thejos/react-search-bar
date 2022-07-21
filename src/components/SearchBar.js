70d10bc584751af7d318b1bd4cd9eb6c8f2ab8d2
dejan.smiljic <dej4n.s@gmail.com>
2022-07-21 19:34:58 +0200

    Modify input element, implement React useState hook

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 function SearchBar() {
-  return <div>This is search bar</div>;
+  //State
+  const [searchValue, setSearchValue] = useState("");
+
+  //Input Change
+  function handleInputChange(event) {
+    // alert(`input changed - [${event.target.value}]`);
+    setSearchValue(event.target.value);
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchValue}
+        placeholder="search..."
+        onChange={handleInputChange}
+      />
+      <div>{searchValue}</div>
+    </div>
+  );
 }
 
 export default SearchBar;
