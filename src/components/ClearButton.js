7aeab80ccb88301486509da5400b2be70668a7e6
dejan.smiljic <dej4n.s@gmail.com>
2022-07-23 18:02:15 +0200

    Add: button react component, clear input field logic, collection of random items, item filtering over user input, show item list

---
+++ src/components/ClearButton.js
@@ -0,0 +1,6 @@
+//React Component
+function ClearButton(props) {
+  return <button onClick={props.clearInput}>Clear</button>;
+}
+
+export default ClearButton;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,38 @@
 import { useState } from "react";
+import ClearButton from "./ClearButton";
+//Imports END
 
+//String array - collection of random items
+const items = [
+  "Clothes Pin",
+  "Tennis Racket",
+  "Snail Shell",
+  "Canteen",
+  "Empty Bottle",
+  "Socks",
+  "Light Bulb",
+  "Fishing Hook",
+];
+
+//React Component
 function SearchBar() {
-  //State
+  //useState React hook
   const [searchValue, setSearchValue] = useState("");
 
-  //Input Change
+  //Get input value and assign it to searchValue variable
   function handleInputChange(event) {
-    // alert(`input changed - [${event.target.value}]`);
     setSearchValue(event.target.value);
   }
+
+  //Clear input via button
+  function clearInput() {
+    setSearchValue("");
+  }
+
+  //Filter items based on user input and store them to filteredItems array
+  const filteredItems = items.filter((item) => {
+    return item.toLowerCase().includes(searchValue.toLowerCase());
+  });
 
   return (
     <div>
@@ -18,9 +42,18 @@ function SearchBar() {
         placeholder="search..."
         onChange={handleInputChange}
       />
+      {/*Conditional rendering (short-circuit conditional)*/}
+      {searchValue.length > 0 && <ClearButton clearInput={clearInput} />}
+      {/* Render input value  */}
       <div>{searchValue}</div>
+      {/* Show (map) every item from filteredItems array */}
+      <ul>
+        {filteredItems.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
     </div>
   );
-}
+} //Component SearchBar END
 
 export default SearchBar;
