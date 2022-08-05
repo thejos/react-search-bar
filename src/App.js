511a8d91d169ab1fa97de5ba5f82ddc0304f334d
dejan.smiljic <dej4n.s@gmail.com>
2022-08-05 15:18:22 +0200

    Fetch data from online REST Api
    - Add another SearchBar component;
    - Show loading spinner while content is loading;
    - Display the content as a list;

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import SearchBar from "./components/SearchBar";
+import { useState, useEffect } from "react";
+import PuffLoader from "react-spinners/PuffLoader";
 
 function App() {
   //String array - collection of random items
@@ -22,10 +24,47 @@ function App() {
     "Lily",
     "Peony",
   ];
+
+  //useState React Hook
+  const [products, setProducts] = useState([]);
+  //useEffect React Hook
+  useEffect(() => {
+    //collects products from fakestore
+    fetch("https://fakestoreapi.com/products?limit=8")
+      .then((res) => res.json())
+      .then((hasProduct) => {
+        //maps product titles to products array via setProducts function from useState hook
+        const fetchedProducts = hasProduct.map((product) => {
+          return product.title;
+        });
+        setProducts(fetchedProducts);
+      });
+  }, []); //useEffect() END
+
   return (
     <div>
       <SearchBar itemList={items} placeholderText={"item search..."} />
       <SearchBar itemList={flowers} placeholderText={"flower search..."} />
+      {/*conditional expression with ternary operator:
+          has-product ? render SearchBar : render loading spinner */}
+      {products.length > 0 ? (
+        <SearchBar itemList={products} placeholderText={"product search..."} />
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "18vw",
+            height: "16vw",
+            padding: "10px",
+          }}
+        >
+          <PuffLoader color="#ACB5B9" loading size={80} speedMultiplier={1} />
+          {/* <span>Loading...</span> */}
+        </div>
+      )}
+      {/*conditional ternary END*/}
     </div>
   );
 }
